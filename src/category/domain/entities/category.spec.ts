8db8e1f3d74ac316ee86912891d8a8ecabd8d928
Gabslima121 <glima@vixting.com.br>
2023-02-27 22:48:58 -0300

    finished tests for Category constructor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,12 +1,62 @@
+import { omit } from 'lodash'
 import { Category } from "./category";
 
 describe("Category Unit Tests", () => {
 
   test('constructor of category', () => {
-    const category = new Category({
-      name: 'Movie'
+    let category = new Category({
+      name: 'Movie',
+    })
+    let props = omit(category.props, 'created_at')
+    expect(props).toStrictEqual({
+      name: 'Movie',
+      description: null,
+      is_active: true
+    })
+    expect(category.props.created_at).toBeInstanceOf(Date)
+
+    let created_at = new Date()
+
+    category = new Category({
+      name: 'Documentary',
+      description: 'some description',
+      is_active: false
+    })
+    expect(category.props).toStrictEqual({
+      name: 'Documentary',
+      description: 'some description',
+      is_active: false,
+      created_at
     })
 
-    expect(category.props.name).toBe('Movie')
+
+    category = new Category({
+      name: 'Movie',
+      description: 'other description',
+    })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      description: 'other description',
+    })
+
+    category = new Category({
+      name: 'Movie',
+      is_active: true,
+    })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      is_active: true
+    })
+
+    created_at = new Date()
+    category = new Category({
+      name: 'Movie',
+      created_at
+    })
+
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      created_at,
+    })
   })
 });
